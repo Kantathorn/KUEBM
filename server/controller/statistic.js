04fe5cebc0c33beb509a11ea8bc683c676d7c67e
Knet-MAC <knet-macintosh@kantathorn-wudhiwiriya.local>
2024-03-30 00:07:45 +0700

    Manage Dashboard API

--- server/controller/statistic.js
+++ server/controller/statistic.js
@@ -7,4 +7,31 @@ exports.getNewRequestNumber = (req,res) => {
     }).catch((error) => {
         return res.status(404).json(error)
     })
-} 
+}
+
+// Get Number of Approve Request
+exports.getApproveRequestNumber = (req,res) => {
+    Requests.countDocuments({ request_to: req.user.club,status: "Approve"}).then((result) => {
+        return res.status(200).json(result)
+    }).catch((error) => {
+        return res.status(404).json(error)
+    })
+}
+
+// Get Number of In-use Request
+exports.getInuseRequestNumber = (req,res) => {
+    Requests.countDocuments({ request_to: req.user.club,status: "In-use"}).then((result) => {
+        return res.status(200).json(result)
+    }).catch((error) => {
+        return res.status(404).json(error)
+    })
+}
+
+// Get Number of Returned Request
+exports.getReturnedRequestNumber = (req,res) => {
+    Requests.countDocuments({ request_to: req.user.club,status: "Returned"}).then((result) => {
+        return res.status(200).json(result)
+    }).catch((error) => {
+        return res.status(404).json(error)
+    })
+}
--- server/route/statistic.js
+++ server/route/statistic.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 const { isLoggedIn } = require('./auth')
 
-const { getNewRequestNumber } = require('../controller/statistic')
+const { getNewRequestNumber,getApproveRequestNumber,getInuseRequestNumber,getReturnedRequestNumber } = require('../controller/statistic')
 
 router.get('/request/new',isLoggedIn,getNewRequestNumber)
+router.get('/request/approve',isLoggedIn,getApproveRequestNumber)
+router.get('/request/In_use',isLoggedIn,getInuseRequestNumber)
+router.get('/request/returned',isLoggedIn,getReturnedRequestNumber)
 
 module.exports = router
